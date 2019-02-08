625aeb9ecb506c1c5757094bbc1fddef53b64cf6
amirkhan1092 <amirkhan1092@gmail.com>
2019-02-09 00:47:49 +0530

    Algorithm Modified

--- prime_number_with_algo.py
+++ prime_number_with_algo.py
@@ -1,3 +1,29 @@
+'''
+Step 1: Start
+Step 2: Read n1 and n2 from user.
+Step 3: Initialize variables
+        k=n1
+Step 5: Repeat the steps until k<=n2
+    Step 5.1: Initialize variables
+                flag=1
+                i=2
+    Step 5.2 Repeat the step until i<k
+            Step 5.2.1 If remainder of k÷i equals 0
+                flag←0
+                Go to step 5.3
+            Step 5.2.2 i←i+1
+    Step 5.3: If flag==0
+                Display n is not prime
+              else
+                Display n is prime
+    Step 5.4: k=k+1
+Step 6: Stop
+
+'''
+
+
+
+
 n1=int(input('enter the first input range number '))
 n2=int(input('enter the second input range number '))
 
