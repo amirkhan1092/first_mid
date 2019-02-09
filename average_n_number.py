ae0366dc385efad4e3607cb3de80475547c8a1f3
amirkhan1092 <amirkhan1092@gmail.com>
2019-02-09 14:21:07 +0530

    Fahreheit to celsius

--- average_n_number.py
+++ average_n_number.py
@@ -2,7 +2,8 @@
 n=int(input('enter the number ')) # how many numbers
 s=0 #initialize variable
 for i in range(1,n+1):
-    s+=i
+    number=eval(input('enter the value '))
+    s += number
 
 average_at_number=s/n
 
